3667f98b473ad2a11bdffe7b5c2b3ddf7222e5b3
balaky Félix <balakyatana@gmail.com>
2020-05-25 20:52:24 +0000

    Ajoutez des fichiers projet.

---
+++ epass/modeles/ville.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epass.modeles
+{
+    public class ville
+    {
+        public string Id { set; get;}
+        public string Nom {set; get;}
+    }
+}
