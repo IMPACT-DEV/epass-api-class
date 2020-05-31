1037bc8f5c3024f853fd5dd04dfdfb20332d7392
balaky Félix <balakyatana@gmail.com>
2020-05-31 09:51:56 +0000

    controlleur de compte

---
+++ epass/Controllers/ComptesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using epass.modeles;
+using epass.models;
+
+namespace epass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComptesController : ControllerBase
+    {
+        private readonly CompteContext _context;
+
+        public ComptesController(CompteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comptes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Compte>>> GetComptes()
+        {
+            return await _context.Comptes.ToListAsync();
+        }
+
+        // GET: api/Comptes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Compte>> GetCompte(Guid id)
+        {
+            var compte = await _context.Comptes.FindAsync(id);
+
+            if (compte == null)
+            {
+                return NotFound();
+            }
+
+            return compte;
+        }
+
+        // PUT: api/Comptes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompte(Guid id, Compte compte)
+        {
+            if (id != compte.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(compte).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comptes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Compte>> PostCompte(Compte compte)
+        {
+            _context.Comptes.Add(compte);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof (GetCompte), new { id = compte.Id }, compte);
+        }
+
+        // DELETE: api/Comptes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Compte>> DeleteCompte(Guid id)
+        {
+            var compte = await _context.Comptes.FindAsync(id);
+            if (compte == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comptes.Remove(compte);
+            await _context.SaveChangesAsync();
+
+            return compte;
+        }
+
+        private bool CompteExists(Guid id)
+        {
+            return _context.Comptes.Any(e => e.Id == id);
+        }
+    }
+}
